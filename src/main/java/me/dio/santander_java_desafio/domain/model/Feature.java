a4271b0929e59026ecd686853ac39a8b775c90ce
Julio Cesar <juliocsantos2504@gmail.com>
2025-08-10 18:26:25 -0300

    Implementacao da camada de dominio

---
+++ src/main/java/me/dio/santander_java_desafio/domain/model/Feature.java
@@ -0,0 +1,7 @@
+package me.dio.santander_java_desafio.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem{
+}
---
+++ src/main/java/me/dio/santander_java_desafio/domain/model/News.java
@@ -0,0 +1,7 @@
+package me.dio.santander_java_desafio.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem{
+}
