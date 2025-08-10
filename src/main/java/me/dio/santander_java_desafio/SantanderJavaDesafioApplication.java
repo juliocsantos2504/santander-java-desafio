2961fa8813de6ec8a4d8642a1579643d46c84b55
Julio Cesar <juliocsantos2504@gmail.com>
2025-08-10 16:52:54 -0300

    Initial commit

---
+++ src/main/java/me/dio/santander_java_desafio/SantanderJavaDesafioApplication.java
@@ -0,0 +1,13 @@
+package me.dio.santander_java_desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderJavaDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderJavaDesafioApplication.class, args);
+	}
+
+}
